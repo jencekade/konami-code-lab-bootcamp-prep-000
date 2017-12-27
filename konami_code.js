4af4d1e4806ce85cfdf5faf04cea82c3562b7d0b
jencekade <gaephuoc@gmail.com>
2017-12-27 09:21:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-/*
+
 function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(e) {
@@ -16,8 +16,9 @@ function init() {
     }
   });
 }
-init();*/
+init();
 
+/* commented out
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
@@ -65,3 +66,4 @@ function init() {
   }
 }
 init();
+*/
