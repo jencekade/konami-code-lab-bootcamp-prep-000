71978089046c178780904710ec5c6c490828c94f
jencekade <gaephuoc@gmail.com>
2017-12-27 09:18:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,26 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 /*
 function init() {
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (key === code[code.length-1]) {
+        alert(`This is the Konami Code!`);
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+}
+init();*/
+
+function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  function onKeyDownHandler(e){
+  document.body.addEventListener('keydown', function(e){
     let key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
@@ -47,20 +64,5 @@ function init() {
     } else {index = 0;}
   }
 }
-*/
-function init() {
-  let index = 0;
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index++;
-      if (key === code[code.length-1]) {
-        alert(`This is the Konami Code!`);
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  });
-}
-init();
+
+
